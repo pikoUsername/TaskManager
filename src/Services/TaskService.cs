9f39e1e27511a87f01cb7ce1e6952957612df44a
pikoUsername <galymzhan.amantaj@gmail.com>
2024-03-11 22:48:34 +0600

    Working on refactoring

---
+++ src/Services/TaskService.cs
@@ -0,0 +1,15 @@
+﻿using TaskManager.Database;
+
+namespace TaskManager.Services
+{
+    public class TaskService 
+    {
+        private TaskManagerContext dbContext; 
+
+        public TaskService(TaskManagerContext dbContext) { 
+            dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async 
+    }
+}
